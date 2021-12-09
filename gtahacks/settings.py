bf2d20e9b91f86963fce441e2c46a32fb5ceabc4
Christian Stefaniw <christian.stefaniw@student.tdsb.on.ca>
2021-12-09 16:11:24 -0500

    test

--- gtahacks/settings.py
+++ gtahacks/settings.py
@@ -16,7 +16,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = []
 
